a51443d8aaffa17e779c49d8037a66e030de024a
Mikhail Shilkov <mikhail@poolside.ai>
2026-02-06 12:34:56 +0100

    Fix __init__.py export for renamed step

--- test_steps/__init__.py
+++ test_steps/__init__.py
@@ -1,5 +1,5 @@
 """Test steps for bridge sidecar elimination prototype."""
 
-from test_steps.steps import hello_code_step, hello_agent_step, multi_turn_api_test
+from test_steps.steps import hello_code_step, hello_agent_step, validated_json_generation
 
-__all__ = ["hello_code_step", "hello_agent_step", "multi_turn_api_test"]
+__all__ = ["hello_code_step", "hello_agent_step", "validated_json_generation"]
